f7dfe0fe20fcbf5a4f92b16951b7a17660a752b2
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-09-19 11:16:08 -0500

    xd

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "google_compute_instance" "app" {
 }
 
 resource "google_compute_firewall" "permitir_http" {
-  name    = "permitir-http"
-  network = google_compute_network.vpc.id
+  name    = "${var.prefijo}-permitir-http"
+  network = google_compute_network.vpc.name
 
   allow {
     protocol = "tcp"
@@ -58,15 +58,17 @@ resource "google_compute_firewall" "permitir_http" {
   target_tags   = ["grupo-2-sv"]
 }
 
-resource "google_compute_firewall" "permitir_ssh" {
-  name    = "permitir-ssh"
-  network = google_compute_network.vpc.id
+resource "google_compute_firewall" "ssh_iap" {
+  name    = "${var.prefijo}-permitir-ssh-iap"
+  network = google_compute_network.vpc.name
 
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  # 35.235.240.0/20 es el rango desde el que Google reenvía SSH
+  # a través de IAP. Es el único origen autorizado para el 22.
+  source_ranges = ["35.235.240.0/20"]
   target_tags   = ["grupo-2-sv"]
 }
